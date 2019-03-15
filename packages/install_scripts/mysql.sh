3b9ea0af92fa14dc665a28f8e4749f85d0c11674
ryanauger <ryan.auger@m2mdtech.com>
2019-03-15 13:25:10 -0700

    added mysql install, updated i3 config, added apt packages, and a polybar install

---
+++ packages/install_scripts/mysql.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt install mysql-server
+mysql_secure_installation
--- packages/install_scripts/polybar.sh
+++ packages/install_scripts/polybar.sh
@@ -1,4 +1,18 @@
 
-packages=("install" "python-xcbgen" "linxcb-ewmh-dev" "libxcb-icccm4-dev" "libxcb1-dev" \
+#!/usr/bin/env bash
+
+set -e 
+
+packages=("python-xcbgen" "linxcb-ewmh-dev" "libxcb-icccm4-dev" "libxcb1-dev" \
     "xcb-proto" "libxcb-util-dev" "libxcb-image0-dev" "libxcb-randr0-dev" "libxcb-xkb-dev" \
    "libalsaplayer-dev" "wireless-tools" "libcurlpp-dev" "libcairo2-dev" )
+
+sudo apt install python-xcbgen libxcb-ewmh-dev libxcb-icccm4-dev libxcb1-dev \
+    xcb-proto libxcb-util-dev libxcb-image0-dev libxcb-randr0-dev libxcb-xkb-dev \
+   libalsaplayer-dev wireless-tools libcurlpp-dev libcairo2-dev libcurl4-dev libmpdclient-dev \
+	libiw-dev libpulse-dev libxcb-composite0-dev
+
+cd /tmp
+git clone https://github.com/jaagr/polybar.git
+cd polybar
+./build.sh
